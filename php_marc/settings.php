a1a8d2d49ec03f56003d84adf5d4621fdc7e4197
Heinz <Heinz@Heinz-PC.fritz.box>
2020-03-23 13:04:52 +0100

    can now configure to display DDC description

--- php_marc/settings.php
+++ php_marc/settings.php
@@ -20,14 +20,18 @@ foreach ($fNames as $name) {
     }
     $opt[] = "<option $sel value='$dir/$name'>$name</option>";
 }
-
+$c = '';
+if ($param->ddcflag) {
+    $c = 'checked';
+}
 
 $echo = [];
 $echo[] = "  <div id=mvsettings style='text-align:left' class='findMe' data-event=keyup data-funame='marc.keyup'>";
 $echo[] = "<label for=m21><b>File</b>:</label><br><select tabindex=1 id=m21 name=file size=1>" . implode('', $opt) . "</select><br>";
 $echo[] = "<label for=fil><b>Filter</b>; list of tags to display</label><br><input  tabindex=2 id=fil name=filter type=text value='$param->filter' size=40 maxlength=256><br>";
-$echo[] = "<label for=rnum><b>Record number</b>; starts with 0</label><br><input  tabindex=3 id=rnum data-type=n  name=recnum type=text value='$param->recnum' size=5 maxlength=256><br>";
-$echo[] = "<label for=rpp><b>Records per page</b>:</label><br><input  tabindex=4  id=rpp data-type=n  name=rpv type=text value='$param->rpv' size=2 maxlength=2><br>&nbsp;";
+$echo[] = "<label for=ddc><b>Explain DDC;</b> </label><br> <input tabindex=3 id=ddc name=ddcflag type=checkbox $c><p>";
+$echo[] = "<label for=rnum><b>Record number</b>; starts with 0</label><br><input  tabindex=4 id=rnum data-type=n  name=recnum type=text value='$param->recnum' size=5 maxlength=256><br>";
+$echo[] = "<label for=rpp><b>Records per page</b>:</label><br><input  tabindex=5  id=rpp data-type=n  name=rpv type=text value='$param->rpv' size=2 maxlength=2><br>&nbsp;";
 $echo[] = "<hr><button  tabindex=5 class='findMe' data-event='click' data-funame='marc.saveSettings' >"
         . "<i class='fas fa-save'></i><span style='font-size:0.8em'> Save </span></button>";
 $echo[] = "</div><br>";
